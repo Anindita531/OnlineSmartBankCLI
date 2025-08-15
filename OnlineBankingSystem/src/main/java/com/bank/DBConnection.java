20ae688b734dde42efb316569ccaf43276e2327c
Anindita531 <aninditaghosh3551@gmail.com>
2025-08-15 17:34:38 +0530

    Add files via upload

--- OnlineBankingSystem/src/main/java/com/bank/DBConnection.java
+++ OnlineBankingSystem/src/main/java/com/bank/DBConnection.java
@@ -5,10 +5,9 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/banking_system";
     private static final String USER = "root"; // your MySQL username
-    private static final String PASSWORD = ""; // your MySQL password
+    private static final String PASSWORD = "Welcome2025!"; // your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
-
